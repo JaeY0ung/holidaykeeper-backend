e7f58bc97e8794a3b2b9ee40f6f982adc2c4fc13
JaeYoungJung <hanol98@naver.com>
2025-12-03 13:13:33 +0900

    init: 프로젝트 생성 및 readme 파일 생성

---
+++ src/main/java/org/planitsquare/holidaykeeper/holidaykeeperbackend/HolidaykeeperBackendApplication.java
@@ -0,0 +1,14 @@
+package org.planitsquare.holidaykeeper.holidaykeeperbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaykeeperBackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HolidaykeeperBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/planitsquare/holidaykeeper/holidaykeeperbackend/HolidaykeeperBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package org.planitsquare.holidaykeeper.holidaykeeperbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidaykeeperBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
